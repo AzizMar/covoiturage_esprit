bd436cec622217dd256f24993dc74c8d7a404a93
Aziz Marzouki <azizxmar@gmail.com>
2019-01-21 17:40:35 +0100

    added template blocks and bg patterns, replaced local fa5 with cdn.

--- covoiturage_esprit/settings.py
+++ covoiturage_esprit/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_FILES = [STATIC_DIR, ]
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
